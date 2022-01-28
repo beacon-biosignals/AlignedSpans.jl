a8749b6dd2f8910b53156e12ba46fb64d3aaa1c1
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2022-01-28 12:43:16 -0500

    wip

--- src/IndicesVsSpans.jl
+++ src/IndicesVsSpans.jl
@@ -1,5 +1,113 @@
 module IndicesVsSpans
 
-# Write your package code here.
+using TimeSpans, Dates
+
+export RoundInward, RoundConstantSamples
+export AlignedSpan
+
+const RoundInward = RoundingMode{:Inward}()
+const RoundConstantSamples = RoundingMode{:ConstantSamples}()
+
+struct AlignedSpan
+    sample_rate::Float64
+    i::Int
+    j::Int
+    function AlignedSpan(sample_rate, i, j)
+        if j < i
+            throw(ArgumentError("TODO"))
+        end
+        return new(sample_rate, i, j)
+    end
+end
+
+TimeSpans.istimespan(::AlignedSpan) = true
+TimeSpans.start(span::AlignedSpan) = TimeSpans.time_from_index(span.sample_rate, span.i)
+TimeSpans.stop(span::AlignedSpan) = TimeSpans.time_from_index(span.sample_rate, span.j) + Nanosecond(1) # exclusive stop
+
+function TimeSpans.index_from_time(sample_rate, span::AlignedSpan)
+    if sample_rate != span.sample_rate
+        throw(ArgumentError("TODO"))
+    end
+    return span.i : span.j
+end
+
+function float_index_from_time(sample_rate, sample_time)
+    time_in_nanoseconds = convert(Nanosecond, sample_time).value
+    time_in_nanoseconds >= 0 ||
+        throw(ArgumentError("`sample_time` must be >= 0 nanoseconds"))
+    time_in_seconds = time_in_nanoseconds / TimeSpans.NS_IN_SEC
+    index = time_in_seconds * sample_rate + 1
+    return index
+end
+
+# Helper to get the index and the rounding error in units of time
+function index_and_error_from_time(sample_rate, sample_time::Period, mode::RoundingMode)
+    f = float_index_from_time(sample_rate, sample_time)
+    index = round(Int, f, mode)
+    return index, TimeSpans.time_from_index(sample_rate, index) - sample_time
+end
+
+# function exact_index_from_time(sample_rate, sample_time::Period)
+#     index, error = index_and_error_from_time(sample_rate, sample_time, RoundNearest)
+
+#     # we allow a 1 ns error, since the TimeSpans's constructor will introduce that
+#     if !(abs(error) <= Nanosecond(1))
+#         # We recompute `f`, but only on the error path
+#         f = float_index_from_time(sample_rate, sample_time)
+#         throw(InexactError(:exact_index_from_time, Int, f))
+#     end
+
+#     return index
+# end
+
+# function exact_index_from_time(sample_rate, span::TimeSpan)
+#     i = exact_index_from_time(sample_rate, start(span))
+#     j = exact_index_from_time(sample_rate, stop(span))
+#     if i != j # do we want this
+#         j -= 1
+#     end
+#     return i:j
+# end
+
+function n_samples(sample_rate, duration::Period)
+    i, _ = index_and_error_from_time(sample_rate, duration, RoundDown)
+    return i - 1
+end
+
+# Returns a subset of the original span, corresponding to the samples it contains
+function AlignedSpan(sample_rate, span::TimeSpan, ::RoundingMode{:Inward})
+    i, _ = index_and_error_from_time(sample_rate, start(span), RoundUp)
+    j, error = index_and_error_from_time(sample_rate, stop(span), RoundDown)
+    if error == Nanosecond(0)
+        # We must exclude the right endpoint
+        j -= 1
+    end
+    if j < i
+        throw(ArgumentError("No samples lie within `span`"))
+    end
+    return AlignedSpan(sample_rate, i, j)
+end
+
+
+# Returns a `span` whose left endpoint is rounded down to the nearest sample,
+# whose length is always the same number of samples (depending only on the duration, not the position, of the span)
+function AlignedSpan(sample_rate, span::TimeSpan, ::RoundingMode{:ConstantSamples})
+    i, _ = index_and_error_from_time(sample_rate, start(span), RoundDown)
+    n = n_samples(sample_rate, duration(span))
+    j = i + (n-1)
+    return AlignedSpan(sample_rate, i, j)
+end
+
+# Round both endpoints down, excluding the right endpoint.
+# Matches the indices from `TimeSpans.index_from_time(sample_rate, span)`
+function AlignedSpan(sample_rate, span::TimeSpan, ::RoundingMode{:Down})
+    i, _ = index_and_error_from_time(sample_rate, start(span), RoundDown)
+    j, error = index_and_error_from_time(sample_rate, stop(span), RoundDown)
+    if i != j && error == Nanosecond(0)
+        # We must exclude the right endpoint
+        j -= 1
+    end
+    return AlignedSpan(sample_rate, i, j)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,49 @@
 using IndicesVsSpans
 using Test
+using TimeSpans, Dates
 
 @testset "IndicesVsSpans.jl" begin
-    # Write your tests here.
+    @testset "RoundInward" begin
+        for span in (TimeSpan(Millisecond(1500), Millisecond(2500)),
+                    TimeSpan(Millisecond(1001), Millisecond(2001)),
+                    TimeSpan(Millisecond(1001), Millisecond(2999)),
+                    TimeSpan(Millisecond(2000), Millisecond(2001)))
+            # all spans include only sample 3, at second 2
+
+            aligned = AlignedSpan(1, span, RoundInward)
+            # Only sample included inside `span` is sample 3
+            @test TimeSpans.index_from_time(1, aligned) == 3:3
+            @test TimeSpan(aligned) == TimeSpan(Second(2), Second(2))
+        end
+
+        # Does *not* contain any samples
+        span = TimeSpan(Millisecond(1999), Millisecond(2000))
+        @test_throws ArgumentError("No samples lie within `span`") AlignedSpan(1, span, RoundInward)
+        
+    end
+
+    @testset "RoundConstantSamples" begin
+        span = TimeSpan(Millisecond(1500), Millisecond(2500))
+        aligned = AlignedSpan(1, span, RoundConstantSamples)
+        inds = TimeSpans.index_from_time(1, aligned)
+        @test length(inds) == IndicesVsSpans.n_samples(1, duration(span))
+        @test inds == 2:2
+        for t = 1:100
+            translated = translate(span, Second(t))
+            aligned = AlignedSpan(1, translated, RoundConstantSamples)
+            inds = TimeSpans.index_from_time(1, aligned)
+            @test length(inds) == IndicesVsSpans.n_samples(1, duration(span))
+        end
+
+        span = TimeSpan(Millisecond(1500), Millisecond(2600))
+        aligned = AlignedSpan(1, span, RoundConstantSamples)
+        @test TimeSpans.index_from_time(1, aligned) == 2:2
+
+        # We should get the same number of samples no matter how we translate it
+        for t in [Millisecond(1):Millisecond(1):Millisecond(1000); Nanosecond(10):Nanosecond(10):Nanosecond(1000)]
+            translated_span = TimeSpans.translate(span, t)
+            aligned = AlignedSpan(1, translated_span, RoundConstantSamples)
+            @test length(TimeSpans.index_from_time(1, aligned)) == length(2:2)
+        end
+    end
 end
